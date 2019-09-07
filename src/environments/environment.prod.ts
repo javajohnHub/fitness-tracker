d61e407e632861da7acd959eeb5d1557c715adee
John Edwards <joedw1978@gmail.com>
2019-09-06 20:56:52 -0400

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD4-8mRpFmz7eRmTkwC_B10Giq1DgALVgs",
+    authDomain: "fitness-d7796.firebaseapp.com",
+    databaseURL: "https://fitness-d7796.firebaseio.com",
+    projectId: "fitness-d7796",
+    storageBucket: "fitness-d7796.appspot.com",
+    messagingSenderId: "541835693046",
+    appId: "1:541835693046:web:3bc3fb149a1cfb5fcb95d7"
+  }
 };
